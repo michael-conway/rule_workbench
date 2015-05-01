5a6d271c66ef461af2b3cfb106cac5519d8aa811
michael-conway <michael_conway@unc.edu>
2015-05-01 15:41:35 -0400

    add skeleton for rule from reagan

--- validate_vocab.r
+++ validate_vocab.r
@@ -1,3 +1,10 @@
 # validate attributes against HIVE vocabularies
+validateDataObjectAgainstOntology {
+
+msiCurlGetStr(*url, *outStr);
+writeLine("stdout", *outStr);
+}
+INPUT *url="http://localhost:8080/hive-voccabservice-rest-1.0-SNAPSHOT/rest/concept/uat/concept?uri=http%3A%2F%2Fpurl.org%2Fastronomy%2Fuat%23T100"
+OUTPUT ruleExecOut
 
 
